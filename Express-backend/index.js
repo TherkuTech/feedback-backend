b2d318e6a935154055a14e256d88179128b3ad02
Cibiyanna <pkcibiyanna@gmail.com>
2024-07-01 02:10:09 +0530

    working to resolve cors

--- Express-backend/index.js
+++ Express-backend/index.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({  
-  origin: ['http://localhost:5173','https://feedworks.netlify.app/']
+  origin: "*"
 }))
 
 app.use(bodyParser.json())
